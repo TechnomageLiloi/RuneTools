1ebd54260d88c739de10d9ffda3e89bea0cedbc9
Anton Moskalenko <moskalenko.anton@gmail.com>
2022-12-17 06:30:27 +0200

    [:0:0:1] Add database adapter to manager.

--- Sources/Manager.php
+++ Sources/Manager.php
@@ -2,10 +2,45 @@
 
 namespace Liloi\Tools;
 
+use Liloi\Config\Pool;
+use Judex\Exceptions\GeneralException;
+
 /**
  * Abstract manager.
  */
 class Manager
 {
+    /**
+     * Configuration pool.
+     * `null` if not given.
+     *
+     * @var Pool|null
+     */
+    private static ?Pool $configuration = null;
 
+    /**
+     * Gets configuration pool.
+     *
+     * @return Pool Configuration pool.
+     */
+    public static function getConfiguration(): Pool
+    {
+        if(is_null(self::$configuration))
+        {
+            // @ToDo: [rune:judex:] Add NotDefinedException
+            throw new GeneralException('Configuration is not defined.');
+        }
+
+        return self::$configuration;
+    }
+
+    /**
+     * Gets configuration pool.
+     * @param Pool $configuration
+     */
+    public static function setConfig(Pool $configuration): void
+    {
+        // @ToDo: [rune:config:] Pool must not be fully static.
+        self::$configuration = $configuration;
+    }
 }
