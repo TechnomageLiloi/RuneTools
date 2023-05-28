9f524865cd5d511d82f90eadd0760e7e2e6baedd
Anton Moskalenko <moskalenko.anton@gmail.com>
2023-05-28 21:24:01 +0300

    [0.0.7] Add tests manager.

---
+++ Tests.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Rune Tools Tests</title>
+</head>
+
+<div style="background-color: black; color: white;padding: 5px;margin: 0px;">
+    <?php echo str_replace("\n", "<br/>", shell_exec('phpunit')); ?>
+</div>
+
+<hr/>
+
+<body>
+</body>
+</html>
